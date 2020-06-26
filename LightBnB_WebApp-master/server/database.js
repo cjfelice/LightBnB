429b2f24d1a4c777356baf592701334f7950f423
cjfelice <monocularmedia@gmail.com>
2020-06-26 08:19:16 +0000

    addproperty sql added

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -163,10 +163,15 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(object) {
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;
+  `, [object.owner_id, object.title, object.description, object.thumbnail_photo_url, object.cover_photo_url, object.cost_per_night, object.parking_spaces, object.number_of_bathrooms, object.number_of_bedrooms, object.country, object.street, object.city, object.province, object.post_code])
+    .then(res => res.rows[0]);
 };
 exports.addProperty = addProperty;
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code) 
+VALUES (1, 'Hellhole', 'Big!', 'url', 'url', 300, 2, 1, 16, 'Canada', 'Rue leFleur', 'Cold Lake', 'Alberta', 'f5r3wd'),
