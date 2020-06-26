3a1cf0c4349cc970889f5a2dd4b2cfe676851290
cjfelice <monocularmedia@gmail.com>
2020-06-26 08:57:38 +0000

    refactored for testing and diagnostics

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,7 +1,4 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,6 +6,39 @@ const pool = new Pool({
   host: 'localhost',
   database: 'lightbnb'
 });
+
+module.exports = {
+  query: (text, params, callback) => {
+    const start = Date.now();
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start;
+      console.log('executed query', { text, duration, rows: res.rowCount });
+      callback(err, res);
+    });
+  },
+  getClient: (callback) => {
+    pool.connect((err, client, done) => {
+      const query = client.query;
+      client.query = (...args) => {
+        client.lastQuery = args;
+        return query.apply(client, args);
+      };
+      const timeout = setTimeout(() => {
+        console.error('A client has been checked out for more than 5 seconds!');
+        console.error(`The last executed query on this client was: ${client.lastQuery}`);
+      }, 5000);
+      const release = (err) => {
+        done(err);
+        clearTimeout(timeout);
+        client.query = query;
+      };
+      callback(err, client, release);
+    });
+  }
+};
+
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
 
 /// Users
 /**
